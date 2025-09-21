092ab161a1d84a8061a51b199c7b71bc6ce64c25
Szucs Adam <adamon940@gmail.com>
2025-09-21 18:47:58 +0200

    Képanyagok hozzáadva.

--- backEnd/controller/admin.controller.js
+++ backEnd/controller/admin.controller.js
@@ -1,7 +1,5 @@
 //import { PrismaClient } from './generated/prisma/index.js'
 import express from "express";
-import cors from "cors";
-
 
 const router = express.Router();
 //const prisma = new PrismaClient();
