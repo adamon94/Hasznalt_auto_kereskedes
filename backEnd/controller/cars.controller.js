53bb98d0323c8dd12dc880bd5cae256e32f0713e
Szucs Adam <adamon940@gmail.com>
2025-10-01 20:42:02 +0200

    Pár request hozzáadása

--- backEnd/controller/cars.controller.js
+++ backEnd/controller/cars.controller.js
@@ -1,14 +1,22 @@
-//import { PrismaClient } from './generated/prisma/index.js'
+import { PrismaClient } from '../generated/prisma/index.js'
 import express from "express";
 
 
+const p = new PrismaClient();
+
 const router = express.Router();
-//const prisma = new PrismaClient();
+
 
 router.get("/getCars", async (req, res) => {
 
-
-  
+    const data  = await p.autok.findMany();
+    res.json(data).status(200)
 });
+/*  Kedvenc autó felvétele  */
+/*  1 felhasználó és egy kedvenc autója közti kapcsolat lekérdezése  */
+/*tesztvezetés időpont felvétele*/
+/*tesztvezetés időpontok lekérdezése*/
+/*Kívánt autó felvétele*/
+/*Azok lekérdezése*/
 
 export { router };
--- backEnd/controller/users.controller.js
+++ backEnd/controller/users.controller.js
@@ -11,24 +11,41 @@ router.get("/getUsers", async (req, res) => {
 });
 
 router.post("/registration", async (req,res) => {
-    const {name, password, accessLevell,email,telSzam} = req.body
-    try{
-         await p.users.create({
+    const {name, password, email,telSzam} = req.body
+    
+    if (name.trim("") === "" || email.trim("") === ""|| password.trim("") === "" ){
+
+        res.status(400).json({message:"Sikertelen regisztráció, kérem töltse ki az összes adatot!"})
+        return;
+    }
+    
+        const data = await p.users.create({
         data:{
             
             name,
             passsword: createHash(password),
-            accessLevel: Number(accessLevell),
+            accessLevel: 0,
             email,
             telSzam
         }
            
-    })
-    res.status(201).json({message :"created"})
-    } catch(err){
-        console.error(err)
-        res.status(400)
-    }
+    });
+    res.json({Message:"Created"}).status(201)
+})
+
+router.get("/login", async (req,res)=> {
+        const {inputName,inputPassword} = req.body; 
+
+        const data = await p.users.findFirst({
+                where:{
+                    name:inputName,
+                    passsword:createHash(inputPassword)
+                }
+        })
+        if(data){
+                res.status(200).json({id:data.id,isSuccess:true})
+        }
+        res.status(400).json({isSuccess:false});
 })
 
 export { router };
